519a919968f4b5e06803f501c64e1eb240be880f
Rafael-Benicio <jerobenicio@gmail.com>
2025-02-15 15:48:55 -0300

    refact: move content to separated file

---
+++ src/rag_content.py
@@ -0,0 +1,25 @@
+def load_raw_content() -> []:
+    return [
+        {
+            "id": 0,
+            "text": """
+        É um conceito muito estudado  na psicologia, que diz que se uma pessoa já fez algo que considera bom ou positivo, ela tem uma probabilidade menor de fazer algo positivo logo a seguir
+
+        EX : "Você enquanto anda na rua dá dois reais a um mendigo, se outro mendigo de abordar posteriormente, você tem uma probabilidade menor de dar dois reais a esse segundo mendigo
+        """
+        },
+        {
+            "id": 1,
+            "text": """
+            Competitividade é uma variável que incorpora fatores, geralmente associados com o desejo de excelência, em comparação com outros, pelo prazer de competir
+        """
+        },
+        {
+            "id": 2,
+            "text": """
+            A liquidez das relações, também diz respeito a destruição de protocolos sociais relacionados a criação de laços sociais
+
+            Como no caso do mundo contemporâneo, onde os protocolos de frequência e presença foram modificados, de modo que diferente de uma rede social, em uma interação presencial você tem mais dificuldade em se "Desconectar
+        """
+        }
+    ]
